aacfe54411e864e21b201ca9cd2bf1ce175ed644
Alex Young <Alex Young@LAPTOP-NK65MP9N>
2021-12-11 14:42:07 -0500

    Submit

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(600, 600);
   }
 
   /** 
@@ -16,21 +16,71 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
-  }
+	    background(255,250,250);
+	    
+	    // White background
+	    noStroke();
+	    fill(40, 50, 200);
+	    rect(0, 0, width, height/2);
+	    
+	    stroke(0);
+	  }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
+	  /**
+	   * Called repeatedly, anything drawn to the screen goes here
+	   */
+	  public void draw() {
+		  
+		  // Bottom snow ball
+		  fill(255,250,250);
+		  ellipse(width/2, height/2, (float) (width * 0.375), (float) (height * 0.375));
+		  
+		  // Middle snow ball
+		  fill(255,250,250);
+		  ellipse(width/2, height/4, width/4, height/4);
+		  
+		  // Top snow ball
+		  fill(255,250,250);
+		  ellipse(width/2, height/8, (float) (width * 0.1875), (float) (height * 0.1875));
+		  
+		  // Left eye
+		  fill(0,0,0);
+		  ellipse((float) (width * 0.475), height/10, width/80, height/80);
+		  
+		  // Right eye
+		  fill(0,0,0);
+		  ellipse((float) (width * 0.5375), height/10, width/80, height/80);
+		  
+		  // Bottom button
+		  fill(0,0,0);
+		  ellipse(width/2, (float) (height * 0.5375), (float) (width * 0.0375), (float) (height * 0.0375));
+		  
+		  // Middle button 
+		  fill(0,0,0);
+		  ellipse(width/2, (float) (height * 0.45), (float) (width * 0.0375), (float) (height * 0.0375));
+		  
+		  // Top button
+		  fill(0,0,0);
+		  ellipse(width/2, (float) (height * 0.275), (float) (width * 0.0375), (float) (height * 0.0375));
+		  
+		  // Smile 
+		  fill(0,0,0);
+		  arc(width/2, height/8, width/40, height/40, 0, PI); 
+		  
+		  // Sun
+		  fill(255,255,0);
+		  ellipse((float) (width * 0.975), height/40, width/8, height/8);
+		  
+		  // Left arm
+		  line(width/8, height/8, (float) (width * 0.375), height/4);
+		  
+		  // Right arm
+		  line((float) (height * 0.625), height/4, (float) (width * 0.875), height/8);
+		  
+		  
+		 
+	  }
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
-  
-  // define other methods down here.
-}
+	  
+	  
+	}
